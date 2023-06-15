79da90b1f51777b55493dafb5ec68a5947e43fef
komalgs11 <komalpatil26.kp@gmail.com>
2023-06-15 18:47:47 +0200

    added some changes in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 let pokemonList = [ // array of objects ,three keys to each Pokémon object.
     {
         name: 'Bulbasaur',
-        height: 7,
+        height: 0.7,
         types: ['grass', 'poison']
     },
     {
         name: 'Pikachu',
-        height: 4,
+        height: 0.4,
         types: ['electric']
     },
     {
         name:'Squirtle',
-        height: 5,
+        height: 0.5,
         type: ['water']
     },
     {
         name: 'Wigglytuff',
         height: 1,
         type: ['fairy','normal']
-    },
+    }
 ];
+
+// document.write() Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”
+for (let i = 0; i < pokemonList.length; i++){ 
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+    if (pokemonList[i].height > 0.8){
+        document.write('<p> --Wow! that’s big pokemon!</p>');
+    }else{
+        document.write('<p> --Wow! this pokemon is cute.</p>');
+    }
+}
+
