b5a0bc05f556caa1854c1103e7eeefe87042e3a7
komalgs11 <komalpatil26.kp@gmail.com>
2023-06-21 00:39:24 +0200

    updated data in java script

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,51 @@
-let pokemonList = [ // array of objects ,three keys to each Pokémon object.
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        types: ['electric']
-    },
-    {
-        name:'Squirtle',
-        height: 0.5,
-        type: ['water']
-    },
-    {
-        name: 'Wigglytuff',
-        height: 1,
-        type: ['fairy','normal']
+// wrap pokemonList array in an IIFE
+let pokemonRepository = (function () {
+    let pokemonList = [ // array of objects ,three keys to each Pokémon object.
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['electric']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            type: ['water']
+        },
+        {
+            name: 'Wigglytuff',
+            height: 1,
+            type: ['fairy', 'normal']
+        }
+    ];
+    // getAll return all items 
+    function getAll() {
+        return pokemonList;
     }
-];
 
-pokemonList.forEach(function(pokemon) {
-    document.write(pokemon.name + " (height: "  + pokemon.height + ") <br>");
-  });
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+pokemonRepository.add(
+    {
+        name: 'Leafeon',
+        height: 1,
+        type: ['grass']
+    });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") <br>");
+});
