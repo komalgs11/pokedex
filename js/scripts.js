7ca237832b4f7645516d00a6fffc0b02dac11c4d
komalgs11 <komalpatil26.kp@gmail.com>
2023-06-20 22:31:04 +0200

    added forEach function in js

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,6 @@ let pokemonList = [ // array of objects ,three keys to each Pokémon object.
     }
 ];
 
-// document.write() Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”
-for (let i = 0; i < pokemonList.length; i++){ 
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
-    if (pokemonList[i].height > 0.8){
-        document.write('<p> --Wow! that’s big pokemon!</p>');
-    }else{
-        document.write('<p> --Wow! this pokemon is cute.</p>');
-    }
-}
-
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + " (height: "  + pokemon.height + ") <br>");
+  });
